a542f4364c1288bedd5a21d9f14119aae0585fc1
SeeXWH <mihaandre1@yamdex.ru>
2024-02-24 19:47:34 +0300

    qwe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        System.out.println(lastName + ' ' + firstName + ' ' + middleName);
+        String FIO = (lastName + ' ' + firstName + ' ' + middleName).toUpperCase();
+        System.out.println(FIO);
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println(fullName);
     }
 }
